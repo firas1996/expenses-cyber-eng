b21fc3544a5babe8a438e78b7660188b62dbe002
firas hamila <firas.hm.1996@gmail.com>
2024-10-17 11:34:07 +0200

    aa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import ExpenseItem from "./Components/ExpenseItem.jsx";
 function App() {
+  const expensesData = [
+    {
+      id: "e1",
+      title: "Education",
+      price: 4455,
+      date: new Date(2024, 9, 17),
+    },
+    {
+      id: "e2",
+      title: "New Phone",
+      price: 5432,
+      date: new Date(2023, 6, 5),
+    },
+    {
+      id: "e3",
+      title: "Food",
+      price: 1234,
+      date: new Date(2024, 6, 1),
+    },
+    {
+      id: "e4",
+      title: "New PC",
+      price: 3333,
+      date: new Date(2022, 3, 11),
+    },
+  ];
   return (
     <>
       <ExpenseItem />
--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import "./ExpenseItem.css";
 const ExpenseItem = () => {
   return (
-    <div>
+    <div className="expense-item">
       <div>date</div>
-      <div>
+      <div className="expense-item__description">
         <h2>title</h2>
-        <div>price</div>
+        <div className="expense-item__price">price</div>
       </div>
     </div>
   );
