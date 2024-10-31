aadf7802cc504400230a086d65753dbda7a391ec
firas hamila <firas.hm.1996@gmail.com>
2024-10-31 10:36:18 +0100

    aaa

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -4,6 +4,12 @@ const ExpenseItem = ({ title, price, date }) => {
   const month = date.toLocaleString("en-US", { month: "long" });
   const day = date.toLocaleString("en-US", { day: "2-digit" });
   const year = date.getFullYear();
+  let newTitle = title;
+  const handelClick = () => {
+    console.log(newTitle);
+    newTitle = "New Title !!!";
+    console.log(newTitle);
+  };
   return (
     <div className="expense-item">
       <div className="expense-date">
@@ -12,7 +18,8 @@ const ExpenseItem = ({ title, price, date }) => {
         <div className="expense-date__day">{day}</div>
       </div>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{newTitle}</h2>
+        <button onClick={handelClick}>Update Title !</button>
         <div className="expense-item__price"> $ {price}</div>
       </div>
     </div>
